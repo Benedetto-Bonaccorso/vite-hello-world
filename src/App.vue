c2d5fd60626f854fc531da0759cc979a580c50d8
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-11-09 16:55:24 +0100

    esercizio completato

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+
+<script>
+  import AppTitle from "./components/AppTitle.vue"
+  export default{
+    components: {
+      AppTitle
+    }
+  }
+  
+</script>
+
+<template>
+  <p>
+    paragrafo in app.vue
+  </p>
+  <AppTitle />
+</template>
+
+<style>
+
+</style>
